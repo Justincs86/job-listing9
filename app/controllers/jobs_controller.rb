d6521cd3c415095c39f636a4cea0ef2a4bb25c93
justin <justinng@Justins-MacBook-Air.local>
2017-07-06 23:54:07 +0800

    implemetn job CRUD

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,53 @@
+class JobsController < ApplicationController
+  def index
+    @jobs = Job.all
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      flash[:notice] = "Job Created"
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      flash[:notice] = "Update Successful"
+      redirect_to jobs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    flash[:alert] = "Job Deleted"
+    redirect_to jobs_path
+  end
+
+
+  private
+
+  def job_params
+    params.requier(:job).permit(:title, :description)
+  end
+
+
+end
