2300da64be3758771603eec5da35ad5f5dc8e8b9
justin <justinng@Justins-MacBook-Air.local>
2017-07-07 00:56:04 +0800

    add wage to job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,9 @@
 class Job < ApplicationRecord
   validates :title, presence: true
 
+  validates_presence_of :wage_lower_bound, :wage_upper_bound, presence: true
+  validates :wage_lower_bound, numericality: { greater_than: 0 }
+
   def is_admin?
     is_admin
   end
