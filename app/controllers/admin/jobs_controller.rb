4b29bfff9333a90abc1f37d3c49d132ceef0032c
justin <justinng@Justins-MacBook-Air.local>
2017-07-07 09:40:35 +0800

    add admin sidebar layout

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,7 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [ :new, :create, :edit, :update, :destroy, :index ]
   before_action :require_is_admin
+  layout "admin"
 
   def index
     @jobs = Job.all
